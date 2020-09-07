1a30f6e134f862fc297707f375a0e0cadc61ab03
dgv <dgvargas@protonmail.com>
2020-09-06 23:58:34 -0300

    added parametrized port/project from env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
 
 	"cloud.google.com/go/datastore"
 )
@@ -11,8 +12,12 @@ var datastoreClient *datastore.Client
 
 func main() {
 	ctx := context.Background()
-	datastoreClient, _ = datastore.NewClient(ctx, "go-vim")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	datastoreClient, _ = datastore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
 }
