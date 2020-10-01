dc3dd32b677fd999d69ec33ba641e3898e0ba55c
Daniel Gomes Vargas <hello@dgv.dev.br>
2020-09-30 21:13:33 -0300

    reviewed share function

--- share.go
+++ share.go
@@ -6,8 +6,7 @@ package main
 
 import (
 	"bytes"
-	"context"
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 	"io"
@@ -17,20 +16,30 @@ import (
 	"google.golang.org/appengine/log"
 )
 
-const salt = "[replace this with something unique]"
+const (
+	salt           = "Go playground salt\n"
+	maxSnippetSize = 64 * 1024
+)
 
 type Snippet struct {
-	Body []byte
+	Body []byte `datastore:",noindex"`
 }
 
 func (s *Snippet) Id() string {
-	h := sha1.New()
+	h := sha256.New()
 	io.WriteString(h, salt)
 	h.Write(s.Body)
 	sum := h.Sum(nil)
 	b := make([]byte, base64.URLEncoding.EncodedLen(len(sum)))
 	base64.URLEncoding.Encode(b, sum)
-	return string(b)[:10]
+	// Web sites don’t always linkify a trailing underscore, making it seem like
+	// the link is broken. If there is an underscore at the end of the substring,
+	// extend it until there is not.
+	hashLen := 11
+	for hashLen <= len(b) && b[hashLen-1] == '_' {
+		hashLen++
+	}
+	return string(b)[:hashLen]
 }
 
 func init() {
@@ -42,12 +51,11 @@ func share(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Forbidden", http.StatusForbidden)
 		return
 	}
-	ctx := context.Background()
 
 	var body bytes.Buffer
-	_, err := body.ReadFrom(r.Body)
+	_, err := io.Copy(&body, io.LimitReader(r.Body, maxSnippetSize+1))
 	if err != nil {
-		log.Errorf(ctx, "reading Body: %v", err)
+		log.Errorf(r.Context(), "reading Body: %v", err)
 		http.Error(w, "Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -55,9 +63,9 @@ func share(w http.ResponseWriter, r *http.Request) {
 	snip := &Snippet{Body: body.Bytes()}
 	id := snip.Id()
 	key := datastore.NameKey("Snippet", id, nil)
-	_, err = datastoreClient.Put(ctx, key, snip)
+	_, err = datastoreClient.Put(r.Context(), key, snip)
 	if err != nil {
-		log.Errorf(ctx, "putting Snippet: %v", err)
+		log.Errorf(r.Context(), "putting Snippet: %v", err)
 		http.Error(w, "Server Error", http.StatusInternalServerError)
 		return
 	}
